158e84e459bb1829cff23639c12d8a7dd9b6e71b
straningera <a.straninger@gmail.com>
2023-10-20 19:43:48 +0200

    ui-r0ck5

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,35 +1,66 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
+import { HashRouter, Link, Route, Routes } from "react-router-dom";
+import { Donor } from "./pages/Donor/Donor";
+import { Restaurant } from "./pages/Restaurant/Restaurant";
+import { Receiver } from "./pages/Receiver/Receiver";
+import { Register } from "./pages/Register/Register";
+import { Landing } from "./pages/Landing/Landing";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+  IconButton,
+  Box,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div style={{ position: "relative" }}>
+      <HashRouter>
+        <Box position={"absolute"} right={5} top={0}>
+          <Menu>
+            <MenuButton
+              as={IconButton}
+              aria-label="Options"
+              icon={<HamburgerIcon />}
+              variant="outline"
+            />
+            <MenuList>
+              <MenuItem as={Link} to="/">
+                Home
+              </MenuItem>
+              <MenuItem as={Link} to="/register">
+                Register
+              </MenuItem>
+              <MenuItem as={Link} to="/restaurant">
+                Restaurant
+              </MenuItem>
+              <MenuItem as={Link} to="/donor">
+                Donor
+              </MenuItem>
+              <MenuItem as={Link} to="/receiver">
+                Receiver
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </Box>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/restaurant" element={<Restaurant />} />
+          <Route path="/donor" element={<Donor />} />
+          <Route path="/receiver" element={<Receiver />} />
+        </Routes>
+      </HashRouter>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ packages/frontend/src/pages/Register/Register.tsx
@@ -0,0 +1,10 @@
+export const Register = () => {
+  return (
+    <div>
+      <h2>Register</h2>
+      <ul>
+        <li>TODO</li>
+      </ul>
+    </div>
+  );
+};
