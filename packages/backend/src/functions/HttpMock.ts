448d14c683f3d6b67bf56cc17383856a394cdc1a
tranguyen21 <84319438+tranguyen21@users.noreply.github.com>
2023-10-21 14:10:53 +0200

    schema adjustment based on frontend requirements

--- packages/backend/src/functions/HttpMock.ts
+++ packages/backend/src/functions/HttpMock.ts
@@ -4,44 +4,48 @@ import {
   HttpResponseInit,
   InvocationContext,
 } from "@azure/functions";
-import * as locations from "../../mocks/locations.json";
-import * as donors from "../../mocks/donors.json"
-import * as issuers from "../../mocks/isIssue.json"
-import * as receivers from "../../mocks/receiver.json"
-import * as products from "../../mocks/products.json";
-import { components } from "jtabac-schema"
 
+import { components } from "jtabac-schema"
+import { createDatabaseEntry, getDatabaseEntry } from "../service/service";
+
+// Products can be added by organizers
+// Reservations can be made by receivers
+// Donations can be made by donors
+
+// thoughts on the database/schema:
+// users are identified by their email address
+
+export type Product = components["schemas"]["Product"];
+export type Order = components["schemas"]["Order"];
+export type User = components["schemas"]["User"];
+export type Location = components["schemas"]["Location"];
 
 export async function HttpMock(
   request: HttpRequest,
   context: InvocationContext
 ): Promise<HttpResponseInit> {
-  context.log(`Http mock data fetched`);
-  const category = request.params.category;
-  console.log(category);
-  //mock data
-  if (category === "products") {
-    const mockData = products;
-    return { jsonBody: mockData };
+  try {
+    if(request.method === "GET"){
+      const data = await getDatabaseEntry(request.params.category as "orders" | "products" | "users" | "locations" | "undefined");
+      return {
+        status: 200,
+        jsonBody: data
+      }
+    }
+    if(request.method === "POST"){
+      await createDatabaseEntry(request.body as Product | Order | User | Location);
+      return {
+        status: 200,
+        jsonBody: {}
+      }
+    }
+  } catch (error) {
+    context.error(error);
+    return {
+      status: 500,
+      body: error.message,
+    };
   }
-  if (category === "locations") {
-    const mockData = locations;
-    return { jsonBody: mockData };
-  }
-
-  if (category === "donors") {
-    const mockData = donors;
-    return { jsonBody: mockData };
-  }
-  if (category === "issuers") {
-    const mockData = issuers;
-    return { jsonBody: mockData };
-  }
-  if (category === "receivers") {
-    const mockData = receivers;
-    return { jsonBody: mockData };
-  }
-  return;
 }
 
 app.http("HttpMock", {
